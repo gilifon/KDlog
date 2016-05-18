164fe5fc8b797337d68f7aa3f08fe1b66966a7da
Gil <gilifon@gmail.com>
2016-05-18 15:37:51 +0300

    loading..

--- App/viewmodels/shell.js
+++ App/viewmodels/shell.js
@@ -31,7 +31,7 @@
             return router.activate();
         },
         compositionComplete: function () {
-            
+            $("#loadingImage").hide();
         }
     };
 });
--- Server/GetLogForCall.php
+++ Server/GetLogForCall.php
@@ -10,16 +10,23 @@ $info = $_POST ["info"];
 // extract all the properties of the request
 if (isset ( $info ['call'] )) {
 	$call = $info ['call'];
-	if ($call == "*all*")
+	if (strtolower($call) == "*all*")
 	{
 		$result = mysql_query("select `call`,`band`,`freq`,`mode`,`qso_date`,`time_on`, `wwff_ref` from log order by `qso_date` desc ,`time_on` desc ") or die('Error: ' . mysql_error());
-		}
+	}
+	//else if(substr( $call, 0, 1 ) === "*")
+	//{
+//		$call = substr( $call, 1, strlen($call)-1)
+		//$result = mysql_query("select `call`,`band`,`freq`,`mode`,`qso_date`,`time_on`, `wwff_ref` from log where `call` = '$call' order by `qso_date` desc ,`time_on` desc ") or die('Error: ' . mysql_error());
+	//}
 	else
 	{
 		$result = mysql_query("select `call`,`band`,`freq`,`mode`,`qso_date`,`time_on`, `wwff_ref` from log where `call` = '$call' order by `qso_date` desc ,`time_on` desc ") or die('Error: ' . mysql_error());
 		mysql_query("insert into audit (`call`) values ('$call')");
-		}
-} else {
+	}
+} 
+else 
+{
 	$result = '';
 }
 while($obj = mysql_fetch_object($result)) {
